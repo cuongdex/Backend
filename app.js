1ec2d18658af70d521f7a1b113ea0bb028a35fa4
cuongdex <sonvancuong2003@gmail.com>
2024-09-19 16:57:45 +0700

    new commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const cors = require("cors")
-const contactsRouter = require("./app/routes/contact.route")
 const ApiError = require("./app/api-error");
+const contactsRouter = require("./app/routes/contact.route")
 
 const app = express();
 
@@ -17,10 +17,10 @@ app.use((req, res, next) => {
     return next(new ApiError(404, "Resource not found"));
 });
 
-app.use((err, req, res, next) => {
-    return res.status(error.statusCode || 500).json({
-        message: error.message || "Internal Sever Error",
-    });
-});
+// app.use((err, req, res, next) => {
+//     return res.status(error.statusCode || 500).json({
+//         message: error.message || "Internal Sever Error",
+//     });
+// });
 
 module.exports = app;
